c537da7d1b39472835350952ff8ccd60cdadf7fb
Casey <spoocter@gmail.com>
2023-06-07 17:07:13 -0400

    Running 404 Handler; Sequelize Error Handler Added, not tested

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -16,11 +17,13 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 
+
 // Security Middleware
 if (!isProduction) {
     // enable cors only in development
     app.use(cors());
   }
+
 
   // helmet helps set a variety of headers to better secure your app
   app.use(
@@ -28,6 +31,7 @@ if (!isProduction) {
       policy: "cross-origin"
     })
   );
+
 
   // Set the _csrf token and create req.csrfToken method
   app.use(
@@ -40,7 +44,46 @@ if (!isProduction) {
     })
   );
 
+
   const routes = require('./routes');
   app.use(routes);
+
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+  });
+
+
+  // Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      let errors = {};
+      for (let error of err.errors) {
+        errors[error.path] = error.message;
+      }
+      err.title = 'Validation error';
+      err.errors = errors;
+    }
+    next(err);
+  });
+
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
 
   module.exports = app;
