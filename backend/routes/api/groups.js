93b4040d4a598f0f1ccfa76dbbc6c140aa45dcac
Casey <spoocter@gmail.com>
2023-06-14 16:37:46 -0400

    Added authentication and autherization where needed

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -2,13 +2,13 @@ const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 const { check } = require('express-validator');
-const { groupEnums, states, handleValidationErrors } = require('../../utils')
+const { groupEnums, states, handleValidationErrors, requireAuth } = require('../../utils')
 const { Group, GroupImage, User, sequelize } = require('../../db/models');
 
 const router = express.Router();
 
 
-router.get('/current', async (req, res) => {
+router.get('/current', requireAuth, async (req, res) => {
     let Groups = await req.user.getGroups();
     for (let i = 0; i < Groups.length; i++) {
         let mmbrs = await Groups[i].countUsers();
@@ -47,10 +47,11 @@ const validateNewGroup = [
     handleValidationErrors
 ];
 
-router.post('/', validateNewGroup, async (req, res) => {
+router.post('/', [requireAuth, validateNewGroup], async (req, res) => {
     let { name, about, type, private, city, state } = req.body;
     console.log({ name, type, private, city, state, about })
-    let newGroup = await Group.create({ name, type, private, city, state, about,
+    let newGroup = await Group.create({
+        name, type, private, city, state, about,
         organizerId: req.user.id
     });
     return res.json(newGroup);
@@ -94,28 +95,35 @@ router.get('/:id', async (req, res, next) => {
     res.json(group)
 })
 
-const validateNewImage= [
+const validateNewImage = [
     check('url')
-    .exists({checkFalsy: true})
-    .isURL()
-    .withMessage('Please provide valid image url.'),
+        .exists({ checkFalsy: true })
+        .isURL()
+        .withMessage('Please provide valid image url.'),
     check('preview')
-    .exists({checkFalsy:true})
-    .isBoolean()
-    .withMessage('Please indicate wether this is a preview image (true or false only).'),
+        .exists({ checkFalsy: true })
+        .isBoolean()
+        .withMessage('Please indicate wether this is a preview image (true or false only).'),
     handleValidationErrors
 ]
 
-router.post('/:id/images', validateNewImage, async (req,res,next)=>{
+router.post('/:id/images', [requireAuth, validateNewImage], async (req, res, next) => {
     let group = await Group.findByPk(req.params.id);
     if (!group) {
         let err = new Error('Group could not be found');
         err.status = 404;
         return next(err);
     }
-    let {url,preview}=req.body;
-    let img = await group.createGroupImage({url,preview});
-    img=img.toJSON();
+    if (group.organizerId !== req.user.id) {
+        const err = new Error('Forbidden');
+        err.title = 'Forbidden';
+        err.errors = { message: 'Authorization required: Only the group owner can add images' };
+        err.status = 403;
+        return next(err);
+    }
+    let { url, preview } = req.body;
+    let img = await group.createGroupImage({ url, preview });
+    img = img.toJSON();
     let rtrnImg = {};
     rtrnImg.id = img.id;
     rtrnImg.url = img.url;
