2259d1d971ba35eb3bae0dc6586257b9fca6bd94
Casey <spoocter@gmail.com>
2023-06-15 17:49:58 -0400

    full built edit event endpoint

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { check } = require('express-validator');
-const { validStates, handleValidationErrors, requireAuth, groupEnums } = require('../../utils')
+const { handleValidationErrors, requireAuth, eventEnums } = require('../../utils')
 const { Event, Group, Venue, EventImage } = require('../../db/models');
 
 const router = express.Router();
@@ -99,6 +99,106 @@ router.post('/:id/images', [requireAuth, validateNewImage], async (req, res, nex
     delete newImg.dataValues.createdAt;
     res.json(newImg);
 });
+
+const validateNewEvent = [
+    check('venueId')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a venue')
+        .custom(async (value, { req }) => {
+            let venue = await Venue.findByPk(value);
+            if (!venue) throw new Error('Venue does not exist');
+        }),
+    check('name')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 5 })
+        .withMessage('Name must be at least 5 characters'),
+    check('type')
+        .exists({ checkFalsy: true })
+        .isIn(eventEnums)
+        .withMessage(`Type must be ${eventEnums.join(' or ')}`),
+    check('capacity')
+        .exists({ checkFalsy: true })
+        .isInt()
+        .withMessage('capacity must be an integer'),
+    check('price')
+        .exists({ checkFalsy: true })
+        .isDecimal()
+        .withMessage('Price must be decimal')
+        .custom(async (value, { req }) => {
+            value = value.toString().split('.');
+            console.log(Number(value[0])<0)
+            if (value[1].length > 2 || Number(value[0]) < 0) throw new Error('Please provide a valid price')
+        }),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage('Description is required'),
+    check('startDate')
+        .exists({ checkFalsy: true })
+        .withMessage('Start date required')
+        .custom(async (value, { req }) => {
+            try {
+                value = new Date(value);
+            } catch { throw new Error('Please provide a valid date') }
+            let start = value.getTime();
+            let now = new Date();
+            now = now.getTime();
+            if (!(start > now)) throw new Error('Start date must be in the future');
+        }),
+    check('endDate')
+        .exists({ checkFalsy: true })
+        .withMessage('End date required')
+        .custom(async (value, { req }) => {
+            try {
+                value = new Date(value);
+            } catch { throw new Error('Please provide a valid end date') }
+            let end = value.getTime();
+            let start;
+            try {
+                start = new Date(req.body.startDate);
+            } catch { throw new Error('Please provide a valid start date') }
+            start = start.getTime();
+            if (!(end > start)) throw new Error('End date must be after the start date');
+        }),
+    handleValidationErrors
+];
+
+router.put('/:id',[requireAuth,validateNewEvent],async(req,res,next)=>{
+    let event = await Event.findByPk(req.params.id);
+    if (!event) {
+        let err = new Error('Event could not be found');
+        err.status = 404;
+        return next(err);
+    };
+    let group = await event.getGroup();
+    let cohosts = await group.getUsers();
+    cohosts = cohosts.reduce((acc, mmbr) => {
+        if (mmbr.Membership.status === 'co-host') {
+            acc.push(mmbr.id);
+        }
+        return acc;
+    }, []);
+    if (![group.organizerId, ...cohosts].includes(req.user.id)) {
+        const err = new Error('Forbidden');
+        err.title = 'Forbidden';
+        err.errors = { message: 'Authorization required: Can only be done by the group host or a co-host' };
+        err.status = 403;
+        return next(err);
+    }
+    let {venueId,name,type,capacity,price,description,startDate,endDate} = req.body;
+    event.venueId =venueId;
+    event.name =name;
+    event.type =type;
+    event.capacity =capacity;
+    event.price =price;
+    event.description =description;
+    event.startDate =startDate;
+    event.endDate =endDate;
+    await event.save();
+    delete event.dataValues.updatedAt;
+    res.json(event);
+});
+
+
 
 
 
