58d988989bd0291c3ae2bc7761fd9bb783d8dbac
Casey <spoocter@gmail.com>
2023-06-30 17:58:00 -0400

    added modal functionality and updated login to use modal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './index.css';
+import "./index.css";
 
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { restoreCSRF, csrfFetch } from './store/csrf';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ModalProvider, Modal } from "./context/Modal";
+import App from "./App";
+
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
-
-import configureStore from './store';
 
 const store = configureStore();
 
@@ -23,11 +24,14 @@ if (process.env.NODE_ENV !== 'production') {
 
 function Root() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
   );
 }
 
