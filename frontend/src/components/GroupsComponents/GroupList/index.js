36db3145534671d5cd7582f96be7328f1aad9399
Casey <spoocter@gmail.com>
2023-07-08 21:49:53 -0400

    pretty groups list

--- frontend/src/components/GroupsComponents/GroupList/index.js
+++ frontend/src/components/GroupsComponents/GroupList/index.js
@@ -1,39 +1,42 @@
 import { NavLink, useHistory } from 'react-router-dom';
-import './index.css';
+import './GroupList.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getGroups } from '../../../store/groups';
 
-function GroupList(){
+function GroupList() {
     const history = useHistory();
     const dispatch = useDispatch();
-    const groups = useSelector(state=>Object.values(state.groups.allGroups))
-    useEffect(()=>{dispatch(getGroups())},[])
+    const groups = useSelector(state => Object.values(state.groups.allGroups))
+    useEffect(() => { dispatch(getGroups()) }, [])
 
     return (
-        <div>
-            <div>
-            <NavLink to='/events'>
-                Events
-            </NavLink>
-            <text>Groups</text>
-            </div>
-            <text>Groups in Converge</text>
-            <div>
-                {groups.map(group=>{
-                    return(
-                        <div onClick={()=>history.push(`/groups/${group.id}`)}>
-                            <img src={group.previewImage}/>
-                            <div>
-                                <h3>{group.name}</h3>
-                                <h4>{`${group.city}, ${group.state}`}</h4>
-                                <text>{group.about}</text>
-                                <h4>{`${group.numMembers} Members * ${group.private? 'Private':'Public'}`}</h4>
+        <div id='middle'>
+            {/* <div> */}
+            <div id='list'>
+                <p id='hide'>gggg</p>
+                <NavLink to='/events'>
+                    Events
+                </NavLink>
+                <a id='inactive'>Groups</a>
+                <p id='subTitle'>Groups in Converge</p>
+                <div>
+                    {groups.map(group => {
+                        return (
+                            <div className='card' onClick={() => history.push(`/groups/${group.id}`)}>
+                                <img src={group.previewImage} />
+                                <div className='info'>
+                                    <h2>{group.name}</h2>
+                                    <h3>{`${group.city}, ${group.state}`}</h3>
+                                    <p>{group.about}</p>
+                                    <h3>{`${group.numMembers} Members * ${group.private ? 'Private' : 'Public'}`}</h3>
+                                </div>
                             </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
+                </div>
             </div>
+            {/* </div> */}
         </div>
     );
 }
